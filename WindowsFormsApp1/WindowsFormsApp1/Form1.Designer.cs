5d07585c838039dce5bb1964f99e3c198c2a6a67
Spherialqueso <Guimnferreira@gmail.com>
2025-09-06 18:58:10 -0300

    Add files via upload

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,76 +29,67 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.txtnome = new System.Windows.Forms.TextBox();
-            this.lblresultado = new System.Windows.Forms.Label();
-            this.txtsenha = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.listBoxlogados = new System.Windows.Forms.ListBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.button4 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel2.SuspendLayout();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBoxlogados = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtsenha = new System.Windows.Forms.TextBox();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.txtnome = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // panel1
             // 
-            resources.ApplyResources(this.pictureBox1, "pictureBox1");
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.anunimo;
-            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.anunimo;
-            this.pictureBox1.InitialImage = global::WindowsFormsApp1.Properties.Resources.anunimo;
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.backgroundpeak;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtsenha);
+            this.panel1.Controls.Add(this.lblresultado);
+            this.panel1.Controls.Add(this.txtnome);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.panel1.Name = "panel1";
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // txtnome
+            // pictureBox2
             // 
-            this.txtnome.BackColor = System.Drawing.Color.SlateBlue;
-            this.txtnome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            resources.ApplyResources(this.txtnome, "txtnome");
-            this.txtnome.Name = "txtnome";
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.backgroundasset;
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
-            // lblresultado
+            // button1
             // 
-            resources.ApplyResources(this.lblresultado, "lblresultado");
-            this.lblresultado.Name = "lblresultado";
-            this.lblresultado.Click += new System.EventHandler(this.lblresultado_Click);
+            this.button1.BackColor = System.Drawing.Color.LightSteelBlue;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // txtsenha
+            // button2
             // 
-            resources.ApplyResources(this.txtsenha, "txtsenha");
-            this.txtsenha.BackColor = System.Drawing.Color.SlateBlue;
-            this.txtsenha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtsenha.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.txtsenha.Name = "txtsenha";
-            this.txtsenha.TextChanged += new System.EventHandler(this.txtsenha_TextChanged);
-            // 
-            // label2
-            // 
-            resources.ApplyResources(this.label2, "label2");
-            this.label2.Name = "label2";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // button3
-            // 
-            resources.ApplyResources(this.button3, "button3");
-            this.button3.Name = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // label1
-            // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Name = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            this.button2.BackColor = System.Drawing.Color.LightSteelBlue;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // panel2
             // 
@@ -110,12 +101,12 @@
             this.panel2.Name = "panel2";
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
-            // listBoxlogados
+            // button4
             // 
-            this.listBoxlogados.FormattingEnabled = true;
-            resources.ApplyResources(this.listBoxlogados, "listBoxlogados");
-            this.listBoxlogados.Name = "listBoxlogados";
-            this.listBoxlogados.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label4
             // 
@@ -123,43 +114,57 @@
             this.label4.Name = "label4";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // panel1
+            // listBoxlogados
             // 
-            resources.ApplyResources(this.panel1, "panel1");
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.txtsenha);
-            this.panel1.Controls.Add(this.lblresultado);
-            this.panel1.Controls.Add(this.txtnome);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.panel1.Name = "panel1";
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.listBoxlogados.FormattingEnabled = true;
+            resources.ApplyResources(this.listBoxlogados, "listBoxlogados");
+            this.listBoxlogados.Name = "listBoxlogados";
+            this.listBoxlogados.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
-            // button2
+            // label1
             // 
-            resources.ApplyResources(this.button2, "button2");
-            this.button2.Name = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
-            // button1
+            // button3
             // 
-            resources.ApplyResources(this.button1, "button1");
-            this.button1.Name = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // label2
             // 
-            resources.ApplyResources(this.button4, "button4");
-            this.button4.Name = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtsenha
+            // 
+            resources.ApplyResources(this.txtsenha, "txtsenha");
+            this.txtsenha.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.txtsenha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtsenha.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtsenha.Name = "txtsenha";
+            this.txtsenha.TextChanged += new System.EventHandler(this.txtsenha_TextChanged);
+            // 
+            // lblresultado
+            // 
+            resources.ApplyResources(this.lblresultado, "lblresultado");
+            this.lblresultado.BackColor = System.Drawing.Color.Transparent;
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Click += new System.EventHandler(this.lblresultado_Click);
+            // 
+            // txtnome
+            // 
+            this.txtnome.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.txtnome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtnome, "txtnome");
+            this.txtnome.Name = "txtnome";
             // 
             // Form1
             // 
@@ -169,19 +174,17 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox txtnome;
         private System.Windows.Forms.Label lblresultado;
         private System.Windows.Forms.TextBox txtsenha;
@@ -195,6 +198,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
